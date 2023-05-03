65baa52e258c6faf32242db57043d26df8eefc65
polfarreburgos <pol.farre.burgos@estudiantat.upc.edu>
2023-05-04 01:19:03 +0200

    Mostrar eventos en el mapa i la seva info amb callout

--- app/components/map/map.tsx
+++ app/components/map/map.tsx
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import * as Location from 'expo-location';
+import axios from 'axios';
 
 export default function Mapa({ latitud, longitud }) {
-//Agafem la info dels eventos:
-  const [eventos, setEventos] = useState([]);
-
   const [region, setRegion] = useState({
     latitude: latitud,
     longitude: longitud,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
+
+  const [eventos, setEventos] = useState([]);
+  const [eventosMarkers, setEventosMarkers] = useState([]);
+  const [showCallout, setShowCallout] = useState(false);
+
+  const SERVER_URL = 'http://192.168.8.101:8080';
 
   useEffect(() => {
     // Obtiene la ubicación actual del dispositivo y actualiza la región del mapa
@@ -31,25 +35,71 @@ export default function Mapa({ latitud, longitud }) {
       });
     })();
 
-    // Aquí se obtiene la información de los eventos de una fuente externa
-    // y se guarda en el estado de eventos
+    // Obtiene la información de los eventos y convierte las direcciones en coordenadas de latitud y longitud
     const obtenerEventos = async () => {
-        const respuesta = await fetch('https://ejemplo.com/eventos');
-        const datos = await respuesta.json();
-        setEventos(datos);
+      try {
+        const response = await axios.get(`${SERVER_URL}/events`);
+        const eventosData = response.data.events;
+        const eventosMarkersData = await Promise.all(
+          eventosData.map(async (evento) => {
+            const direccion = evento.adress;
+            const response = await axios.get(
+              `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+                direccion
+              )}&key=AIzaSyCj7cgW6zZL9aN1gEZtRXxjM3jcsDXIKTQ`
+            );
+            const resultado = response.data.results[0];
+            if (!resultado) {
+              return null;
+            }
+            const { lat, lng } = resultado.geometry.location;
+            const dataIni: Date = new Date(evento.dataIni);
+            const data: string = dataIni.toLocaleDateString();
+            return { latitude: lat, longitude: lng, title: evento.title, denominacio: evento.denominacio, dataIni: data, adress: evento.adress, horari: evento.horari};
+          })
+        );
+        setEventosMarkers(eventosMarkersData.filter(Boolean));
+        setEventos(eventosData);
+      } catch (error) {
+        console.log(error);
+      }
     };
-    
+
     obtenerEventos();
 
   }, []);
 
+  const closeCallout = () => {
+    setShowCallout(false);
+  };
+
   return (
     <View style={styles.container}>
       <MapView style={styles.map} region={region}>
-      {eventos.map(evento => (
-        <Marker key={evento.id} coordinate={{ latitude: evento.latitud, longitude: evento.longitud }} />
-      ))}
+        {eventosMarkers.map((marker) => (
+          <Marker 
+            key={`${marker.latitude}-${marker.longitude}`}
+            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+            title={marker.title}>
+            <Callout style={styles.callout}>
+              <View>
+                <Text style={styles.eventName}>{marker.denominacio}</Text>
+                <Text style={styles.eventDescription}>{marker.dataIni}</Text>
+                <Text style={styles.eventDescription}>{marker.horari}</Text>
+                <Text style={styles.eventDescription}>{marker.adress}</Text>
+              </View>
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>¡Apúntate!</Text>
+              </TouchableOpacity>
+            </Callout>
+          </Marker>
+        ))}
       </MapView>
+      {showCallout && (
+        <View style={styles.calloutOverlay}>
+          <TouchableOpacity style={styles.calloutOverlayTouchable} onPress={closeCallout} />
+        </View>
+      )}
     </View>
   );
 }
@@ -57,11 +107,37 @@ export default function Mapa({ latitud, longitud }) {
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    flex:1,
+    flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  eventName: {
+    fontWeight: 'bold',
+  },
+  eventDescription: {
+    flex: 1
+  },
+  callout: {
+    padding: 10,
+    minWidth: 150,
+  },
+  button: {
+    alignSelf: 'flex-end',
+    backgroundColor: '#34b38a',
+    borderBottomRightRadius: 16,
+    bottom: 0,
+    paddingHorizontal: 9,
+    marginBottom: -5,
+    paddingVertical: 1,
+    position: 'absolute',
+    right: -5
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 12,
+    fontWeight: 'bold',
   },
 });
