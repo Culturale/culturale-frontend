bd9a55b634f8053266726726fb29d8d5409d8787
polfarreburgos <pol.farre.burgos@estudiantat.upc.edu>
2023-05-12 02:12:42 +0200

    Vamos avanzando... :)

--- app/views/map-screen/map-screen.styles.ts
+++ app/views/map-screen/map-screen.styles.ts
@@ -1,69 +1,112 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export const MapScreenStyles = StyleSheet.create({
-    container: {
-        ...StyleSheet.absoluteFillObject,
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    titleContainer: {
-        paddingTop: 50,
-        paddingBottom: 5,
-        paddingHorizontal: 32,
-        backgroundColor: '#34b38a',
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: 'bold',
-        color: '#fff',
-    },
-    bottomContainer: {
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        padding: 5,
-        width: '100%',
-    },
-    subTitle: {
-        fontSize: 15,
-        fontWeight: 'bold',
-    },
-    map: {
-        ...StyleSheet.absoluteFillObject,
-        flex: 1,
-        marginBottom: 10,
-    },
-    eventName: {
-        fontWeight: 'bold',
-    },
-    eventDescription: {
-        flex: 1,
-    },
-    callout: {
-        flex: 1,
-        minWidth: 150,
-    },
-    button: {
-        alignSelf: 'flex-end',
-        backgroundColor: '#34b38a',
-        borderBottomRightRadius: 16,
-        bottom: 0,
-        paddingHorizontal: 9,
-        marginBottom: -5,
-        paddingVertical: 1,
-        position: 'absolute',
-        right: -5,
-    },
-    buttonText: {
-        color: '#FFF',
-        fontSize: 12,
-        fontWeight: 'bold',
-    },
-    locationButton: {
-        position: 'absolute',
-        bottom: 16,
-        right: 16,
-        backgroundColor: '#333',
-        padding: 8,
-        borderRadius: 100,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  titleContainer: {
+    paddingTop: 50,
+    paddingBottom: 5,
+    paddingHorizontal: 32,
+    backgroundColor: '#34b38a',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  bottomContainer: {
+    padding: 5,
+  },
+  subTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  map: {
+    zIndex: -1,
+    flex: 1,
+  },
+  calloutContainer: {
+    paddingTop: 20,
+    paddingLeft: 20,
+    paddingRight:20,
+    paddingBottom: 30,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+  },
+  eventName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  eventDescription: {
+    marginBottom: 10,
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  iconContainer: {
+    marginRight: 5,
+  },
+  infoText: {
+    fontSize: 14,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginTop: 10,
+  },
+  button: {
+    right: 23,
+    bottom:-30,
+    paddingHorizontal: 7,
+    paddingVertical: 5,
+    borderRadius: 5,
+    backgroundColor: '#34b38a',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  locationButton: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    padding: 10,
+    borderRadius: 50,
+    backgroundColor: '#333',
+  },
+  searchContainer: {
+    zIndex: 0,
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    right: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingLeft: 10,
+  },
+  searchInput: {
+    flex: 1,
+    height: 40,
+    fontSize: 16,
+    marginRight: 10,
+    color: '#333',
+  },
+  searchButton: {
+    position: 'absolute',
+    zIndex: 1,
+    backgroundColor: '#34b38a',
+    padding: 4,
+    borderRadius: 10,
+    right: 10,
+  },  
 });
--- app/views/map-screen/map-screen.tsx
+++ app/views/map-screen/map-screen.tsx
@@ -2,16 +2,20 @@ import { MapScreenStyles as styles } from './map-screen.styles';
 import { useApplicationLayer } from '~/hooks';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import React, { useState, useEffect } from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
 import * as Location from 'expo-location';
 import { PROVIDER_GOOGLE } from 'react-native-maps';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 export default function MapScreen({ latitud, longitud }) {
+
   const {controllers:{EventController}} = useApplicationLayer();
   const events = EventController.events;
   const [showCallout, setShowCallout] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
     useEffect(() => {
       // Obtiene la ubicación actual del dispositivo y actualiza la región del mapa
       (async () => {
@@ -23,20 +27,21 @@ export default function MapScreen({ latitud, longitud }) {
   
         let location = await Location.getCurrentPositionAsync({});
         console.log("HOLA");
+        //NO ENTRA, NO demana permis per la ubi...
         setRegion({
           latitude: location.coords.latitude,
           longitude: location.coords.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.09,
         });
       })();
     }, []);
 
     const [region, setRegion] = useState({
-        latitude: latitud,
-        longitude: longitud,
-        latitudeDelta: 10.0922,
-        longitudeDelta: 20.0421,
+        latitude: 41.3851,
+        longitude: 2.1734,
+        latitudeDelta: 0.09,
+        longitudeDelta: 0.09,
       });
   
     const closeCallout = () => {
@@ -53,9 +58,16 @@ export default function MapScreen({ latitud, longitud }) {
         setRegion({
           latitude: location.coords.latitude,
           longitude: location.coords.longitude,
-          latitudeDelta: 10.0922,
-          longitudeDelta: 20.0421,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.09,
         });
+    };
+
+    const searchEvents = async () => {
+        const filteredEvents = events.filter((event) =>
+          event.denominacio.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setSearchResults(filteredEvents);
     };
 
   return (
@@ -67,29 +79,88 @@ export default function MapScreen({ latitud, longitud }) {
       <View>
         <Text style={styles.subTitle}>Eventos interesantes cerca de mí...</Text>
       </View>
+      
     </View>
     <View style={styles.map}>
       <MapView style={styles.map}
         region={region}
         provider={PROVIDER_GOOGLE}
         >
+             <View style={styles.searchContainer}>
+        <TextInput
+            style={styles.searchInput}
+            placeholder="Introduce el lugar del evento"
+            placeholderTextColor="#000"
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            onSubmitEditing={searchEvents}
+        />
+        <Ionicons style={{ zIndex: 0 }} name="search" size={24} color="transparent" />
+        <TouchableOpacity style={styles.searchButton} onPress={searchEvents}>
+            <Ionicons style={{ zIndex: 1 }} name="search" size={24} color="white" />
+        </TouchableOpacity>
+    </View>
         {events.map((event) => (
           <Marker 
             key={`${event.lat}-${event.long}`}
             coordinate={{ latitude: event.lat, longitude: event.long}}
             title={event.denominacio}>
-            <Callout style={styles.callout}>
-              <View>
+           <Callout style={styles.calloutContainer}>
+            <View style={styles.container}>
+                <Text style={styles.eventName}>{event.denominacio}</Text>
+                <Text style={styles.subTitle}>{event.descripcio}</Text>
+
+                <View style={styles.infoContainer}>
+                <View style={styles.iconContainer}>
+                    <Ionicons name="time" size={18} color="#888" />
+                </View>
+                <Text style={styles.infoText}>{event.horari}</Text>
+                </View>
+
+                <View style={styles.infoContainer}>
+                <View style={styles.iconContainer}>
+                    <Ionicons name="calendar" size={18} color="#888" />
+                </View>
+                <Text style={styles.infoText}>{event.dataIni.toLocaleDateString()}</Text>
+                </View>
+
+                <View style={styles.infoContainer}>
+                <View style={styles.iconContainer}>
+                    <Ionicons name="location" size={18} color="#888" />
+                </View>
+                <Text style={styles.infoText}>{event.adress}</Text>
+                </View>
+
+                <View style={styles.infoContainer}>
+                    <View style={styles.iconContainer}>
+                        <Ionicons name="card" size={18} color="#888" />
+                    </View>
+                    <Text>{event.price}</Text>
+                    <TouchableOpacity style={styles.button}>
+                     <Text style={styles.buttonText}>¡Apúntate!</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </Callout>
+          </Marker>
+        ))}
+        {searchResults.map((event) => (
+        <Marker 
+            key={`${event.lat}-${event.long}`}
+            coordinate={{ latitude: event.lat, longitude: event.long}}
+            title={event.denominacio}>
+            <Callout style={styles.calloutContainer}>
+            <View>
                 <Text style={styles.eventName}>{event.denominacio}</Text>
                 <Text style={styles.eventDescription}>{event.horari}</Text>
                 <Text style={styles.eventDescription}>{event.dataIni.toLocaleDateString()}</Text>
                 <Text style={styles.eventDescription}>{event.adress}</Text>
-              </View>
-              <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>¡Apúntate!</Text>
-              </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>¡Apúntate!</Text>
+                </TouchableOpacity>
+            </View>
             </Callout>
-          </Marker>
+        </Marker>
         ))}
       </MapView>
       <TouchableOpacity style={styles.locationButton} onPress={onPressLocation}>
@@ -104,3 +175,13 @@ export default function MapScreen({ latitud, longitud }) {
   </View>
   );
 }
+
+/*          
+
+SI poso aixo dins del callout pk es mostri la foto del event, al apretar es tanca la app, peta???
+                <Image
+                source={{uri: event.photo}}
+                style={styles.image}
+                resizeMode="cover"
+                />
+                */
