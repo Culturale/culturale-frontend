1d527a4cc48c5970d47e9179a485b984d0f2a504
Joel <jmaciasrojas@gmail.com>
2023-05-13 10:28:02 +0200

    mergear main

--- app/views/showFriends-screen/showFriends-screen.styles.ts
+++ app/views/showFriends-screen/showFriends-screen.styles.ts
@@ -22,6 +22,14 @@ export const ShowFriendsStyles =  StyleSheet.create({
       fontSize: 16,
       fontWeight: 'bold',
     },
+    input: {
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 4,
+      padding: 10,
+      marginBottom: 10,
+      width: '100%',
+    },
     friendInfo: {
       fontSize: 14,
       color: '#666',
@@ -49,7 +57,11 @@ export const ShowFriendsStyles =  StyleSheet.create({
     },
     shadowOpacity: 0.2,
     shadowRadius: 1.41,
-
+    paddingVertical: 10,
+    borderBottomColor: 'lightgray',
+    borderBottomWidth: 1,
+    justifyContent: 'space-between',
+    marginRight: 10, // ajusta el margen a la derecha
     elevation: 2,
   },
   profilePicture: {
@@ -70,7 +82,22 @@ export const ShowFriendsStyles =  StyleSheet.create({
     width: 40,
     marginRight: 16,
   },
-    
+ 
+  removeButton: {
+    color: 'red',
+    marginLeft: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'red',
+    alignSelf: 'flex-start', // ajusta la alineación del botón
+  },
+  mostraramigo:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  }
+  
 });
 
   
--- app/views/showFriends-screen/showFriends-screen.tsx
+++ app/views/showFriends-screen/showFriends-screen.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
+import { View, Text, Image, ScrollView, TextInput, TouchableOpacity } from 'react-native';
 import { observer } from 'mobx-react-lite';
 import { ShowFriendsStyles as styles } from './showFriends-screen.styles';
 import { IUser } from '~/domain';
 import { useApplicationLayer } from '~/hooks';
 
-
 export const ShowFriendsScreen = observer(() => {
   const {
     controllers: {UserController},
   } = useApplicationLayer();
+
+  const handleRemoveFriend = async (friendId: string) => {
+    // try {
+    //   const response = await fetch(`/users/deleteFollowers/${friendId}`, {
+    //     method: 'PUT',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Authorization': `Bearer ${UserController.userInfo.token}`,
+    //     },
+    //   });
+    //   if (!response.ok) {
+    //     throw new Error('Error al eliminar amigo');
+    //   }
+    //   UserController.removeFriend(friendId);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  }
 
   const amigos: IUser[] = UserController.userInfo.followers.filter(user => {
     const userString = JSON.stringify(user);
@@ -18,8 +35,7 @@ export const ShowFriendsScreen = observer(() => {
       return userString === followedString;
     });
   });
-  //const amigos:IUser[] = UserController.userInfo.friends;
-  
+
   if(amigos.length > 0){
     return (
       <View style={styles.container}>
@@ -27,25 +43,28 @@ export const ShowFriendsScreen = observer(() => {
         <ScrollView style={styles.listContainer}>
           {amigos.map((amigo) => (
             <View key={amigo.username} style={styles.userContainer}>
+              < View style={styles.mostraramigo}>
               <Image src={amigo.profilePicture} style={styles.foto} />
               <Text style={styles.username}>{amigo.username}</Text>
+              </View>
+              <TouchableOpacity onPress={() => handleRemoveFriend(amigo.username)}>
+                <Text style={styles.removeButton}>Eliminar</Text>
+              </TouchableOpacity>
             </View>
           ))}
         </ScrollView>
       </View>
     );
   }
-else{
+  else{
     return ( 
       <View style={styles.container}>
-      <Text style={styles.header}>Mis amigos</Text>
-      <Text style={styles.noFriendsMessage}>
-        <Text >¡Esto está muy vacío!</Text>
-        <Text> Añade a gente nueva para empezar esta aventura </Text>
-      </Text>
-    </View>
-);
-}
-
+        <Text style={styles.header}>Mis amigos</Text>
+        <Text style={styles.noFriendsMessage}>
+          <Text>¡Esto está muy vacío!</Text>
+          <Text> Añade a gente nueva para empezar esta aventura </Text>
+        </Text>
+      </View>
+    );
+  }
 });
-
