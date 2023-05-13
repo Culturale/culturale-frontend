dfc309db18418d80854919ff087e0a5c29829652
polfarreburgos <pol.farre.burgos@estudiantat.upc.edu>
2023-05-13 17:10:39 +0200

    Barra de cerca per ubicar el mapa a una població

--- app/views/map-screen/map-screen.tsx
+++ app/views/map-screen/map-screen.tsx
@@ -64,116 +64,98 @@ export default function MapScreen({ latitud, longitud }) {
     };
 
     const searchEvents = async () => {
-        const filteredEvents = events.filter((event) =>
-          event.denominacio.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        setSearchResults(filteredEvents);
+        const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchTerm}&key=AIzaSyCj7cgW6zZL9aN1gEZtRXxjM3jcsDXIKTQ`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.status === 'OK') {
+            const location = data.results[0].geometry.location;
+            setRegion({
+              latitude: location.lat,
+              longitude: location.lng,
+              latitudeDelta: 0.09,
+              longitudeDelta: 0.09,
+            });
+            setSearchTerm('');
+        }
+        else {
+            alert('No se encontró la ubicación deseada');
+        }
     };
 
   return (
     <View style={styles.container}>
-    <View style={styles.titleContainer}>
-      <Text style={styles.title}>Mapa</Text>
-    </View>
-    <View style={styles.bottomContainer}>
-      <View>
-        <Text style={styles.subTitle}>Eventos interesantes cerca de mí...</Text>
-      </View>
-      
-    </View>
-    <View style={styles.map}>
-      <MapView style={styles.map}
-        region={region}
-        provider={PROVIDER_GOOGLE}
-        >
-             <View style={styles.searchContainer}>
-        <TextInput
-            style={styles.searchInput}
-            placeholder="Introduce el lugar del evento"
-            placeholderTextColor="#000"
-            value={searchTerm}
-            onChangeText={(text) => setSearchTerm(text)}
-            onSubmitEditing={searchEvents}
-        />
-        <Ionicons style={{ zIndex: 0 }} name="search" size={24} color="transparent" />
-        <TouchableOpacity style={styles.searchButton} onPress={searchEvents}>
-            <Ionicons style={{ zIndex: 1 }} name="search" size={24} color="white" />
-        </TouchableOpacity>
-    </View>
-        {events.map((event) => (
-          <Marker 
-            key={`${event.lat}-${event.long}`}
-            coordinate={{ latitude: event.lat, longitude: event.long}}
-            title={event.denominacio}>
-           <Callout style={styles.calloutContainer}>
-            <View style={styles.container}>
-                <Text style={styles.eventName}>{event.denominacio}</Text>
-                <Text style={styles.subTitle}>{event.descripcio}</Text>
-
-                <View style={styles.infoContainer}>
-                <View style={styles.iconContainer}>
-                    <Ionicons name="time" size={18} color="#888" />
-                </View>
-                <Text style={styles.infoText}>{event.horari}</Text>
-                </View>
-
-                <View style={styles.infoContainer}>
-                <View style={styles.iconContainer}>
-                    <Ionicons name="calendar" size={18} color="#888" />
-                </View>
-                <Text style={styles.infoText}>{event.dataIni.toLocaleDateString()}</Text>
-                </View>
-
-                <View style={styles.infoContainer}>
-                <View style={styles.iconContainer}>
-                    <Ionicons name="location" size={18} color="#888" />
-                </View>
-                <Text style={styles.infoText}>{event.adress}</Text>
-                </View>
-
-                <View style={styles.infoContainer}>
-                    <View style={styles.iconContainer}>
-                        <Ionicons name="card" size={18} color="#888" />
-                    </View>
-                    <Text>{event.price}</Text>
-                    <TouchableOpacity style={styles.button}>
-                     <Text style={styles.buttonText}>¡Apúntate!</Text>
+        <View style={styles.titleContainer}>
+            <Text style={styles.title}>Mapa</Text>
+        </View>
+        <View style={styles.bottomContainer}>
+            <View>
+                <Text style={styles.subTitle}>Eventos interesantes cerca de mí...</Text>
+            </View>
+        </View>
+        <View style={styles.map}>
+            <MapView style={styles.map} region={region} provider={PROVIDER_GOOGLE}>
+                <View style={styles.searchContainer}>
+                    <TextInput style={styles.searchInput} placeholder="Introduce el lugar del evento"
+                               placeholderTextColor="#000" value={searchTerm}
+                               onChangeText={(text) => setSearchTerm(text)} onSubmitEditing={searchEvents}/>        
+                    <TouchableOpacity style={styles.searchButton} onPress={searchEvents}>
+                        <Ionicons name="search" size={24} color="white" />
                     </TouchableOpacity>
                 </View>
-            </View>
-        </Callout>
-          </Marker>
-        ))}
-        {searchResults.map((event) => (
-        <Marker 
-            key={`${event.lat}-${event.long}`}
-            coordinate={{ latitude: event.lat, longitude: event.long}}
-            title={event.denominacio}>
-            <Callout style={styles.calloutContainer}>
-            <View>
-                <Text style={styles.eventName}>{event.denominacio}</Text>
-                <Text style={styles.eventDescription}>{event.horari}</Text>
-                <Text style={styles.eventDescription}>{event.dataIni.toLocaleDateString()}</Text>
-                <Text style={styles.eventDescription}>{event.adress}</Text>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>¡Apúntate!</Text>
-                </TouchableOpacity>
-            </View>
-            </Callout>
-        </Marker>
-        ))}
-      </MapView>
-      <TouchableOpacity style={styles.locationButton} onPress={onPressLocation}>
-        <Ionicons name="md-locate" size={24} color="white" />
-      </TouchableOpacity>
-      {showCallout && (
-        <View>
-          <TouchableOpacity onPress={closeCallout} />
+
+                {events.map((event) => (
+                    <Marker key={`${event.lat}-${event.long}`}
+                            coordinate={{ latitude: event.lat, longitude: event.long}}
+                            title={event.denominacio}>
+                        <Callout style={styles.calloutContainer}>
+                            <View style={styles.container}>
+                                <Text style={styles.subTitle}>{event.denominacio}</Text>
+                                <Text style={styles.eventName}>{event.descripcio}</Text>
+                                <View style={styles.infoContainer}>
+                                    <View style={styles.iconContainer}>
+                                        <Ionicons name="time" size={18} color="#888" />
+                                    </View>
+                                    <Text style={styles.infoText}>{event.horari}</Text>
+                                </View>
+                                <View style={styles.infoContainer}>
+                                    <View style={styles.iconContainer}>
+                                        <Ionicons name="calendar" size={18} color="#888" />
+                                    </View>
+                                    <Text style={styles.infoText}>{event.dataIni.toLocaleDateString()}</Text>
+                                </View>
+                                <View style={styles.infoContainer}>
+                                    <View style={styles.iconContainer}>
+                                        <Ionicons name="location" size={18} color="#888" />
+                                    </View>
+                                    <Text style={styles.infoText}>{event.adress}</Text>
+                                </View>
+                                <View style={styles.infoContainer}>
+                                    <View style={styles.iconContainer}>
+                                        <Ionicons name="card" size={18} color="#888" />
+                                    </View>
+                                    <Text>{event.price}</Text>
+                                    <TouchableOpacity style={styles.button}>
+                                        <Text style={styles.buttonText}>¡Apúntate!</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </Callout>
+                    </Marker>
+                ))}
+            </MapView>
+
+            <TouchableOpacity style={styles.locationButton} onPress={onPressLocation}>
+                <Ionicons name="md-locate" size={24} color="white" />
+            </TouchableOpacity>
+
+            {showCallout && (
+                <View>
+                    <TouchableOpacity onPress={closeCallout} />
+                </View>
+            )}
         </View>
-      )}
     </View>
-  </View>
-  );
+    );
 }
 
 /*          
